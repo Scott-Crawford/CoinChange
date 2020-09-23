aeb40eb75dd800b48e2bc3581c1fee33794b2e36
Scott Crawford <scottfcrawford@gmail.com>
2020-09-23 06:27:22 -0700

    Fix spacing

--- src/main/java/coinchange/util/Constants.java
+++ src/main/java/coinchange/util/Constants.java
@@ -2,9 +2,9 @@ package coinchange.util;
 
 //For storing constants that may need to be quickly or frequently changed
 public class Constants {
-    //Special condition is divisible by 3
-    public static final int SPECIAL_CONDITION = 3;
+  //Special condition is divisible by 3
+  public static final int SPECIAL_CONDITION = 3;
 
-    //Default mode is greedy
-    public static final String DEFAULT_MODE = "greedy";
+  //Default mode is greedy
+  public static final String DEFAULT_MODE = "greedy";
 }
